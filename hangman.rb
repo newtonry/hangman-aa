d1bcec185838da2adb7ec68d68a366581239897a
rvkn <fadetocasting@gmail.com>
2013-09-22 14:18:33 -0700

    Created AI that's too slow

--- hangman.rb
+++ hangman.rb
@@ -10,7 +10,10 @@ class Hangman
     
     while true
       num_guesses += 1
-      puts @chooser.guessed_word
+      @chooser.guessed_word
+      @guesser.update_known(@chooser.guessed_word)
+      p @chooser.guessed_word
+      
       
       @chooser.respond( @guesser.guess )
       
@@ -56,9 +59,12 @@ class HumanPlayer
   def fill_in_guessed guessed_places, letter
     guessed_places.split(" ").map(&:to_i).each do |place|
       @guessed_word[place - 1] = letter
-   end
- end
+    end
+  end
 
+  def update_known known_so_far
+    @guessed_word = known_so_far
+  end
 
 
   def get_word
@@ -71,24 +77,79 @@ class HumanPlayer
     return false if !('a'..'z').include?(letter) or @used_letters.include?(letter)
     true
   end
-
-
-
 end
 
 
 class ComputerPlayer
-  attr_reader :guessed_word, :selected_word
+  attr_accessor :guessed_word, :selected_word
 
   def initialize
     @dictionary = load_dictionary
     @selected_word = nil
     @guessed_word = nil
+    @possible_words = nil
+    @used_letters = []
   end
 
 
   def guess
+    letter = select_letter
+    puts "I'm guess #{letter}"
+    @used_letters << letter
+    
+    
+    puts @possible_words
+    
+    letter
   end
+  
+  def select_letter
+    init_possible_words if @possible_words.nil?
+    get_most_freq_letter
+  end
+
+  def init_possible_words
+    @possible_words = @dictionary.select do |word|
+      @guessed_word.length == word.length
+    end    
+  end
+
+  def get_most_freq_letter
+    reduce_possible_words
+    
+    highest_letter_count = [0, nil]
+    letters_left = @possible_words.join('').each_char.select { |letter| !@used_letters.include?(letter) }
+    letters_left.each do |letter|
+      if letters_left.count(letter) > highest_letter_count[0]
+        highest_letter_count = [letters_left.count(letter), letter]
+      end
+    end
+    p highest_letter_count[1], ":     ", highest_letter_count[0]
+    highest_letter_count[1]
+  end
+
+  def reduce_possible_words
+    @possible_words = @possible_words.select do |word|
+      possible_match?(word)
+    end
+  end
+
+  def possible_match? possible_word
+    @guessed_word.each_char.with_index do |char, ind|
+      return false if (char != '_' and char != possible_word[ind])
+      if @used_letters.include?(possible_word[ind]) and char != possible_word[ind]
+        return false
+      end
+    end
+    
+    true
+  end
+
+
+  def update_known known_so_far
+    @guessed_word = known_so_far
+  end
+
 
   def respond letter
     if @selected_word.include?(letter)
@@ -121,7 +182,12 @@ end
 comp = ComputerPlayer.new
 hum = HumanPlayer.new
 
-game = Hangman.new(hum, hum)
+# comp.guessed_word = "_" * 22
+# 
+# puts comp.select_letter
+# puts comp.get_most_freq_letter
+
+game = Hangman.new(hum, comp)
 
 game.play
 
